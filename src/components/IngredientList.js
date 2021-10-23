4287d4d7bf072190785fbd931c6bd49f0256ac80
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-10-23 15:43:40 +0100

    added checkboxes to each list item

--- src/components/IngredientList.js
+++ src/components/IngredientList.js
@@ -1,8 +1,9 @@
+import React, { useState } from 'react';
 
 
-
-function IngredientsList({ ingredients }){
-
+function IngredientsList({ ingredients }) {
+   
+    const [checked, setChecked] = useState(false);
 
     console.log(ingredients);
 
@@ -10,7 +11,8 @@ function IngredientsList({ ingredients }){
         <div>
             <h1>Choose Ingredients Please</h1>
             <ul>
-                {ingredients.map(el => <li>{el.name}</li>)}
+                {ingredients.map(el => <li>{el.name}<input type="checkbox" defaultChecked={checked}onChange={() => setChecked(!checked)}/></li>)}
+
             </ul>
         </div>
         
