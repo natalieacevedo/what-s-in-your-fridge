e0ef424f9f85bd5ee82ed9ca35d392f27f91e13a
Goncalo <mysterio.55@hotmail.com>
2021-10-28 17:37:53 +0100

    added button link and background to homepage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Home from './components/Home';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home";
 import IngredientRoot from "./components/IngredientRoot";
 import Navbar from "./components/Navbar";
-import Recipes from './components/Recipes';
-
-
-
-
-
-
+import Recipes from "./components/Recipes";
+import "./homePage.css";
 
 function App() {
-  
   return (
-      <Router>
-    <div>
-      
-      <Switch>
-      
-      <Route exact path='/'>
-      <Home />
-      </Route>     
-      <Route path='/ingredientRoot'>
-      <IngredientRoot name="natita"/>
-      </Route>     
-      <Route path='/recipes'>
-      <Recipes />
-      </Route>     
-     </Switch>
-    </div>
-      </Router>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/ingredientRoot">
+            <IngredientRoot name="natita" />
+          </Route>
+          <Route path="/recipes">
+            <Recipes />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
