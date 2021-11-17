a2ee2c73c4335b60c1115fb85064021acfd9592d
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-11-17 16:09:27 +0000

    commit before switching branch

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -2,16 +2,20 @@ import { React, useState, useEffect, useContext } from "react";
 import FavoriteContext from "../Context/FavoriteContext";
 
 function Favorite() {
+
+    const arrayWithAllFavorite = []
     
     const { favoriteContent } = useContext(FavoriteContext);
 
     if (favoriteContent['title']) {
         
-        console.log(favoriteContent['title']);
-    }
+    arrayWithAllFavorite.push([favoriteContent['title'], favoriteContent['image'], favoriteContent['sourceUrl']]);
+    
+    };
+
+    console.log(arrayWithAllFavorite);
 
     
-
     return (
 
         <>
