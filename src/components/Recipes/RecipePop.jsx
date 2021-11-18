8632e76324c32fe6a2353bb3d16447852a22f115
Goncalo <mysterio.55@hotmail.com>
2021-11-18 15:39:18 +0000

    added stores logos to recipes

--- src/components/Recipes/RecipePop.jsx
+++ src/components/Recipes/RecipePop.jsx
@@ -1,32 +1,42 @@
-import {React ,useEffect, useState, useContext}  from 'react'
-import { Modal, Button, Image, Container, Stack } from 'react-bootstrap'
-import smiley from '../../images/facefood.png';
-import smileyColor from '../../images/facefoodcolor.png';
-import axios from 'axios';
-import "./Recipes-style.css"
+import { React, useEffect, useState, useContext } from "react";
+import { Modal, Button, Image, Container, Stack } from "react-bootstrap";
+import smiley from "../../images/facefood.png";
+import smileyColor from "../../images/facefoodcolor.png";
+import axios from "axios";
+import "./Recipes-style.css";
 import FavoriteContext from "../Context/FavoriteContext";
+import continente from "../../images/continente.logo.png";
+import auchan from "../../images/auchangelogo.png";
+import pingodoce from "../../images/pingodocelogo.png";
 
-function RecipePop({recipe}) {
+function RecipePop({ recipe }) {
   const [show, setShow] = useState(false);
   const [details, setDetails] = useState([]);
   const [allInfo, setAllInfo] = useState([]);
 
-  const { isFavorite, addFavorite, removeFavorite } = useContext(FavoriteContext);
+  const { isFavorite, addFavorite, removeFavorite } =
+    useContext(FavoriteContext);
   const currentlyFavorite = isFavorite(recipe.id);
-  
- function recipeDetails(id) {
-      axios
-        .get(`http://localhost:5000/api/recipe/${id}`)
-        .then((response) => {
-          let detailsWithNoTags = response.data.summary.replace(/<\/?[^>]+(>|$)/g, "");
-          let indexOfInformationWeDontWant = detailsWithNoTags.indexOf( 'It is brought to you');
-          let finalDetailsString = detailsWithNoTags.slice(0, indexOfInformationWeDontWant);
-          
-          setDetails(finalDetailsString);
-          setAllInfo(response.data);
-        })
-  };
-      
+
+  function recipeDetails(id) {
+    axios.get(`http://localhost:5000/api/recipe/${id}`).then((response) => {
+      let detailsWithNoTags = response.data.summary.replace(
+        /<\/?[^>]+(>|$)/g,
+        ""
+      );
+      let indexOfInformationWeDontWant = detailsWithNoTags.indexOf(
+        "It is brought to you"
+      );
+      let finalDetailsString = detailsWithNoTags.slice(
+        0,
+        indexOfInformationWeDontWant
+      );
+
+      setDetails(finalDetailsString);
+      setAllInfo(response.data);
+    });
+  }
+
   useEffect(() => recipeDetails(recipe.id), [recipe.id]);
 
   const SmileyClick = () => {
@@ -38,49 +48,65 @@ function RecipePop({recipe}) {
     }
   };
 
-    return (
-      <>
-        <Button variant="danger" onClick={() => setShow(true)}>
-          Recipe
-        </Button>
-  
-        <Modal
-          show={show}
-          onHide={() => setShow(false)}
-          size="lg"
-          centered
-        >
-          <Modal.Header closeButton>
-            <Modal.Title >
-              {allInfo.title}
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body >
-            <div className="center-image">
-            <Image src={allInfo.image}  fluid />
-            </div>
-            <p>{details} </p>
-            <p> {allInfo.serving}</p>
-            <p><a href={allInfo.sourceUrl}>Let's make it!</a></p>
-           
-          </Modal.Body>
-          <Container >
+  return (
+    <>
+      <Button variant="danger" onClick={() => setShow(true)}>
+        Recipe
+      </Button>
 
-            <img height='60px' width='60px'
-            src={currentlyFavorite ? smileyColor: smiley} alt="carita" onClick={SmileyClick}></img>
-         
-            
-           <Stack direction="horizontal" gap={5} className="justify-content-center">
-          <Button onClick={()=> window.open("https://www.auchan.pt/", "_blank")}>Auchan</Button>
-          <Button  variant="danger" onClick={()=> window.open("https://www.continente.pt/", "_blank")}>Continente</Button>
-          <Button  variant="success" onClick={()=> window.open("https://www.pingodoce.pt/", "_blank")}>Pingo Doce</Button>
-            </Stack>
-           
-          </Container>
-        </Modal>
-      </>
-    );
-    }
-  
-  
+      <Modal show={show} onHide={() => setShow(false)} size="lg" centered>
+        <Modal.Header closeButton>
+          <Modal.Title>{allInfo.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="center-image">
+            <Image src={allInfo.image} fluid />
+          </div>
+          <p>{details} </p>
+          <p> {allInfo.serving}</p>
+          <p>
+            <a href={allInfo.sourceUrl}>Let's make it!</a>
+          </p>
+        </Modal.Body>
+        <Container>
+          <img
+            height="60px"
+            width="60px"
+            src={currentlyFavorite ? smileyColor : smiley}
+            alt="carita"
+            onClick={SmileyClick}
+          ></img>
+
+          <Stack
+            direction="horizontal"
+            gap={5}
+            className="justify-content-center"
+          >
+            <Button
+              variant="white"
+              onClick={() => window.open("https://www.auchan.pt/", "_blank")}
+            >
+              <img src={auchan} alt="Auchan" width="40%" />
+            </Button>
+            <Button
+              variant="white"
+              onClick={() =>
+                window.open("https://www.continente.pt/", "_blank")
+              }
+            >
+              <img src={continente} alt="Continente" width="40%" />
+            </Button>
+            <Button
+              variant="white"
+              onClick={() => window.open("https://www.pingodoce.pt/", "_blank")}
+            >
+              <img src={pingodoce} alt="Pingo Doce" width="80%" />
+            </Button>
+          </Stack>
+        </Container>
+      </Modal>
+    </>
+  );
+}
+
 export default RecipePop;
