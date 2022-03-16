57402be9051a59a0c2d464b9022d252789b7d772
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-03-16 11:01:54 +0000

    fixed navbar

--- src/components/Favorites/index.jsx
+++ src/components/Favorites/index.jsx
@@ -11,39 +11,37 @@ function Favorite() {
     const { favorites } = useContext(FavoriteContext);
        
     return (
-
-        <>
+      <>
         <Navbar1 />
-          <br></br>    
-          <br></br>    
-              <Container className="favourite-container">
-                {Object.values(favorites).length > 0 ? (
-                    <Row >
-                        {Object.values(favorites).map(el =>
-                            <>
-                                <Card >
-                                    <div className="new-overflow">
-                                        <Card.Img className="custom-overflow" src={el.image} />
-                                    </div>
-                                    <Card.Body className="text-center">
-                                        <Card.Title>{el.title}</Card.Title>
-                                        <Card.Text>
-                                            {el.description}
-                                        </Card.Text>
-                                        <RecipePop recipe={el} />
-                                    </Card.Body>
-                                </Card>
-                            </>
-                        )}
-                    </Row>
-                ) : 
-                <h1>You don't have any favourites yet :(</h1> 
-                
-                }
-              </Container>
-              <Footer />
-          </>
-    )
+        <br></br>
+        <br></br>
+        <Container className="favourite-container">
+          {Object.values(favorites).length > 0 ? (
+            <Row>
+              {Object.values(favorites).map((el) => (
+                <>
+                  <Card>
+                    <div className="new-overflow">
+                      <Card.Img className="custom-overflow" src={el.image} />
+                    </div>
+                    <Card.Body className="text-center">
+                      <Card.Title>{el.title}</Card.Title>
+                      <Card.Text>{el.description}</Card.Text>
+                      <RecipePop recipe={el} />
+                    </Card.Body>
+                  </Card>
+                </>
+              ))}
+            </Row>
+          ) : (
+            <h1 className="no favorites">
+              You don't have any favourites yet :(
+            </h1>
+          )}
+        </Container>
+        <Footer />
+      </>
+    );
 
 
 
--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,52 +3,89 @@ import { NavLink } from "react-router-dom";
 import { Container, Navbar, Nav } from "react-bootstrap";
 import "./Navbar.css";
 
-const Navbar1 = () => {
+// const Navbar1 = () => {
+//   return (
+//     <>
+//       {
+//         <div className="center-buttons-middle">
+//           <a className="btn-nav" href="#">
+//             <span className="wave2"></span>
+//             <span className="text">
+//               <Navbar variant="dark" expand="sm">
+//                 <Container fluid>
+//                   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+//                   <Navbar.Collapse id="responsive-navbar-nav">
+//                     <Nav justify variant="pills" className="m-auto">
+//                       <Nav.Link
+//                         className="my-navbar"
+//                         as={NavLink}
+//                         activeClassName="active"
+//                         to="/ingredients/"
+//                       >
+//                         <h3>Ingredients</h3>
+//                       </Nav.Link>
+//                       <Nav.Link
+//                         as={NavLink}
+//                         activeClassName="active"
+//                         to="/recipes/"
+//                         disabled
+//                       >
+//                         <h3>Recipes</h3>
+//                       </Nav.Link>
+//                       <Nav.Link
+//                         as={NavLink}
+//                         activeClassName="active"
+//                         to="/favorites/"
+//                       >
+//                         <h3>Favorites</h3>
+//                       </Nav.Link>
+//                     </Nav>
+//                   </Navbar.Collapse>
+//                 </Container>
+//               </Navbar>
+//             </span>
+//           </a>
+//         </div>
+//       }
+//     </>
+//   );
+// };
+
+function Navbar1() {
   return (
-    <>
-      {
-        <div className="center-buttons-middle">
-          <a className="btn-nav" href="#">
-            <span className="wave2"></span>
-            <span className="text">
-              <Navbar variant="dark" expand="sm" >
-                <Container fluid>
-                  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                  <Navbar.Collapse id="basic-navbar-nav" >
-                    <Nav  justify variant="pills" className="m-auto">
-                      <Nav.Link
-                        className="my-navbar"
-                        as={NavLink}
-                        activeClassName="active"
-                        to="/ingredients/"
+  <Navbar className="color-nav" variant="light"expand="lg" sticky="top">
+  <Container>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+           
+            <Nav.Link
+            as={NavLink}
+            activeClassName="active"
+            to="/">
+                        <h3>Home</h3>
+            </Nav.Link>
+           
+            <Nav.Link
+            className="my-navbar"
+            as={NavLink}
+            activeClassName="active"
+            to="/ingredients/"
                       >
                         <h3>Ingredients</h3>
                       </Nav.Link>
-                      <Nav.Link
-                        as={NavLink}
-                        activeClassName="active"
-                        to="/recipes/"
-                        disabled
+            <Nav.Link
+              as={NavLink}
+              activeClassName="active"
+              to="/favorites/"
                       >
-                        <h3>Recipes</h3>
+                        <h3>Favorites</h3>
                       </Nav.Link>
-                      <Nav.Link
-                        as={NavLink}
-                        activeClassName="active"
-                        to="/favorites/"
-                      >
-                        <h3>Favourites</h3>
-                      </Nav.Link>
-                    </Nav>
-                  </Navbar.Collapse>
-                </Container>
-              </Navbar>
-            </span>
-          </a>
-        </div>
-      }
-    </>
-  );
+          </Nav>
+      </Navbar.Collapse>
+    </Container>
+    </Navbar>
+  )
 };
 
 export default Navbar1;
